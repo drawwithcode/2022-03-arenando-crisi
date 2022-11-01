5a1738d6426df29a996abcb7c84700288c1cd4c4
scardinanoire <andre.corsi@outlook.it>
2022-11-01 23:07:38 +0100

    bozza1

--- sketch.js
+++ sketch.js
@@ -1,101 +1,194 @@
-//this is the tracker component - it's just a black box don't worry about it
-var ctracker;
+// the library that i used to track the eyes can be found here: https://github.com/auduno/clmtrackr
 
-/* 
-if sound doesn't work click the gear button an select run in browser ON
-*/
-var mic;
-var volume = 0;
+// i've followed this tutorial to use it: https://www.youtube.com/watch?v=kCHpFe4T7_A
+
+// i've recycled the movement pattern of the asteroids from the previous assignment
+
+let webcam = null;
+let ctracker = null;
+let p1x, p1y, p2x, p2y;
+let numast = 10;
+let asteroid = [];
+let distbord = 10;
+let spaceShip = null;
+let angle = 0;
+let shoot;
+let shot = [];
+let launched = false;
+
+function preload() {
+  spaceShip = loadImage("./assets/myspace.png");
+}
 
 function setup() {
-  // setup camera capture
-  var videoInput = createCapture(VIDEO);
-  videoInput.size(800, 600);
-  videoInput.position(0, 0);
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  rectMode(CENTER);
 
-  //you can hide the video
-  //videoInput.hide();
+  webcam = createCapture(VIDEO);
+  webcam.size(400, 300);
+  webcam.hide();
 
-  // setup canvas
-  var cnv = createCanvas(800, 600);
-  cnv.position(0, 0);
-
-  // setup tracker
   ctracker = new clm.tracker();
-  ctracker.init(pModel);
-  ctracker.start(videoInput.elt);
+  ctracker.init();
+  ctracker.start(webcam.elt);
 
-  // Create an Audio input
-  mic = new p5.AudioIn();
-
-  // start the Audio Input.
-  // By default, it does not .connect() (to the computer speakers)
-  mic.start();
-
-  noStroke();
+  for (let i = 0; i < numast; i++) {
+    asteroid.push(
+      new asteroids(
+        random(0, width - distbord),
+        random(0, height - distbord),
+        round(random(30, 45)),
+        random(1, 4)
+      )
+    );
+  }
 }
 
 function draw() {
-  //similar to background but it will make transparent pixels
-  //and will not get rid of the camera feed if not hidden
-  clear();
+  background(0);
 
-  // Get the overall volume (between 0 and 1.0)
-  var v = mic.getLevel();
-  // "Smooth" the volume variable with an easing function
-  volume += (v - volume) / 3;
+  translate(width, 0);
+  scale(-1, 1);
+  //image(webcam, width / 2 - webcam.width / 2, height - webcam.height);
+  var features = ctracker.getCurrentPosition();
 
-  //get the detection score, or how well the face is detected (from 0 to 1)
-  var detectionScore = ctracker.getScore();
+  if (features.length > 0) {
+    let leftEye = features[27];
+    let lEx = leftEye[0];
+    let lEy = leftEye[1];
 
-  if (detectionScore > 0) {
-    // get array of face marker positions [x, y] format
-    var positions = ctracker.getCurrentPosition();
+    let rightEye = features[32];
+    let rEx = rightEye[0];
+    let rEy = rightEye[1];
 
-    /*
-    You can find all the points here
-    https://camo.githubusercontent.com/e967f92904c8ef84228b8950d3a278efb895b9d2/68747470733a2f2f617564756e6f2e6769746875622e696f2f636c6d747261636b722f6578616d706c65732f6d656469612f666163656d6f64656c5f6e756d626572696e675f6e65775f736d616c6c2e706e67
-    */
+    p1x = map(lEx, 0, 400, 0, width);
+    p1y = map(lEy, 0, 300, 0, height);
+    p2x = map(rEx, 0, 400, 0, width);
+    p2y = map(rEy, 0, 300, 0, height);
 
-    var leftEyeX = positions[32][0];
-    var leftEyeY = positions[32][1];
+    /*ellipse(p1x, p1y, 20);
+    ellipse(p2x, p2y, 20);*/
 
-    var rightEyeX = positions[27][0];
-    var rightEyeY = positions[27][1];
+    angle = (Math.atan2(p2y - p1y, p2x - p1x) * 180) / Math.PI;
+  }
 
-    var noseX = positions[62][0];
-    var noseY = positions[62][1];
+  for (let i = 0; i < asteroid.length; i++) {
+    asteroid[i].move();
 
-    var faceLeftX = positions[1][0];
-    var faceLeftY = positions[1][1];
-    var faceRightX = positions[13][0];
-    var faceRightY = positions[13][1];
-
-    //the distance between the left point and the rightmost point is a good indication
-    //of the size of the face or proximity to the camera and I can use it to scale my graphic elements
-    var size = dist(faceLeftX, faceLeftY, faceRightX, faceRightY);
-
-    //draw the face
-    fill(255, 255, 255, 60);
-    ellipse(noseX, noseY, size, size);
-
-    //draw the eyes
-    fill(255);
-    ellipse(rightEyeX, rightEyeY, 10, 10);
-    ellipse(leftEyeX, leftEyeY, 10, 10);
-
-    var noseSize = map(volume, 0, 1, 30, 400);
-    //draw a nose
-    ellipse(noseX, noseY, noseSize, noseSize / 2);
-
-    /////When you click the mouse this draws all the detected positions
-    if (mouseIsPressed) {
-      for (var i = 0; i < positions.length; i++) {
-        // set the color of the ellipse based on position on screen
-        fill(255, 0, 0);
-        // draw ellipse at each position point
-        ellipse(positions[i][0], positions[i][1], 4, 4);
+    for (let z = 0; z < shot.length; z++) {
+      if (
+        asteroid[i].x + 30 > shot[z].x &&
+        asteroid[i].y + 30 > shot[z].y &&
+        asteroid[i].x - 30 < shot[z].x &&
+        asteroid[i].y - 30 < shot[z].y
+      ) {
+        asteroid.splice(i, 1);
+        shot.splice(z, 1);
       }
     }
   }
+
+  push();
+  translate((p1x + p2x) / 2, (p1y + p2y) / 2);
+  rotate(angle);
+  imageMode(CENTER);
+  image(spaceShip, 0, 0, 70, 82);
+  pop();
+
+  for (let i = 0; i < shot.length; i++) {
+    shot[i].move();
+  }
+}
+
+class asteroids {
+  constructor(xiniz, yiniz, amp, velocity) {
+    this.x = xiniz;
+    this.y = yiniz;
+    this.a = amp;
+    this.case = 1;
+    this.time = 0;
+    this.count = 0;
+    this.v = velocity;
+    this.color = "white";
+  }
+
+  move() {
+    noFill();
+    stroke(this.color);
+    rect(this.x, this.y, this.a);
+
+    if (this.count == this.time) {
+      this.time = round(random(100, 200));
+      this.count = 0;
+      this.case = round(random(1, 4));
+    }
+
+    switch (this.case) {
+      case 1:
+        this.x += this.v;
+        this.v = this.v + random(-0.05, 0.05);
+        break;
+      case 2:
+        this.x -= this.v;
+        this.v = this.v + random(-0.05, 0.05);
+        break;
+      case 3:
+        this.y += this.v;
+        this.v = this.v + random(-0.05, 0.05);
+        break;
+      case 4:
+        this.y -= this.v;
+        this.v = this.v + random(-0.05, 0.05);
+        break;
+    }
+
+    if (this.x > width - distbord) {
+      this.case = 2;
+    }
+
+    if (this.x < distbord) {
+      this.case = 1;
+    }
+
+    if (this.y > height - distbord) {
+      this.case = 4;
+    }
+
+    if (this.y < distbord) {
+      this.case = 3;
+    }
+    this.count++;
+  }
+}
+
+class missile {
+  constructor() {
+    this.dir = angle + 90;
+    this.vel = 10;
+    this.x = (p1x + p2x) / 2;
+    this.y = (p1y + p2y) / 2;
+  }
+
+  move() {
+    push();
+    translate(this.x, this.y);
+    rotate(this.dir);
+    rect(0, 0, 20, 10);
+    pop();
+
+    this.x = this.x - this.vel * cos(this.dir);
+    this.y = this.y - this.vel * sin(this.dir);
+  }
+}
+
+function keyPressed() {
+  if (keyCode == 32) {
+    shoot = true;
+    shot.push(new missile());
+  }
+}
+
+function keyReleased() {
+  shoot = false;
 }
